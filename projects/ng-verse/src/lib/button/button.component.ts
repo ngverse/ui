bfad60322ce2d0dc3e48108ee7a964b5c5d6538f
luka onik <luka.onikadze@toptal.com>
2024-12-10 13:11:39 +0400

    feat: button redesign

--- projects/ng-verse/src/lib/button/button.component.ts
+++ projects/ng-verse/src/lib/button/button.component.ts
@@ -1,13 +1,7 @@
 import { NgClass } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
-type COLOR_TYPES =
-  | 'primary'
-  | 'secondary'
-  | 'tertiary'
-  | 'danger'
-  | 'success'
-  | 'warning';
+type COLOR_TYPES = 'primary' | 'secondary' | 'danger' | 'success';
 
 type VARIANT_TYPES = 'fill' | 'outline';
 
@@ -24,7 +18,7 @@ type SIZE_TYPES = 'sm' | 'md' | 'lg';
 export class ButtonComponent {
   color = input<COLOR_TYPES>('primary');
 
-  variant = input<VARIANT_TYPES>('outline');
+  variant = input<VARIANT_TYPES>('fill');
 
   disabled = input<boolean>();
 
