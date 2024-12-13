3dfb673a69bd953d5088bdf9db3519955ef86467
luka onik <luka.onikadze@toptal.com>
2024-12-13 11:55:30 +0400

    tab group implemented

--- projects/ng-verse/src/lib/tab/tab-group/tab-group.component.ts
+++ projects/ng-verse/src/lib/tab/tab-group/tab-group.component.ts
@@ -1,26 +1,21 @@
 import {
-  AfterContentChecked,
-  AfterContentInit,
   afterNextRender,
-  afterRender,
-  afterRenderEffect,
   ChangeDetectionStrategy,
   Component,
   computed,
   contentChildren,
-  effect,
   ElementRef,
-  input,
   model,
   viewChild,
   viewChildren,
 } from '@angular/core';
 import { TabComponent } from '../tab.component';
 import { CdkPortalOutlet } from '@angular/cdk/portal';
+import { CdkListbox, CdkOption } from '@angular/cdk/listbox';
 
 @Component({
   selector: 'app-tab-group',
-  imports: [CdkPortalOutlet],
+  imports: [CdkPortalOutlet, CdkListbox, CdkOption],
   templateUrl: './tab-group.component.html',
   styleUrl: './tab-group.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -33,7 +28,7 @@ export class TabGroupComponent {
 
   tabMainHeader = viewChild('tabMainHeader', { read: ElementRef<HTMLElement> });
 
-  tabInk = viewChildren('tabInk', { read: ElementRef<HTMLElement> });
+  tabInk = viewChild('tabInk', { read: ElementRef<HTMLElement> });
 
   isSelected(tabIndex: number) {
     return this.selectedIndex() === tabIndex;
@@ -47,7 +42,7 @@ export class TabGroupComponent {
           const tabMainHeader = this.tabMainHeader()?.nativeElement;
           const tabHeader = this.tabHeaders()[0].nativeElement;
 
-          const tabInk = this.tabInk()[0]?.nativeElement;
+          const tabInk = this.tabInk()?.nativeElement;
           if (tabHeader && tabMainHeader && tabInk) {
             const tabRect = tabHeader.getBoundingClientRect();
             const tabsRect = tabMainHeader.getBoundingClientRect();
@@ -55,17 +50,30 @@ export class TabGroupComponent {
             tabInk.style.width = `${tabRect.width}px`;
             tabInk.style.left = `${tabRect.left - tabsRect.left}px`;
           }
-        },100);
+        }, 500);
       },
     });
   }
 
   inkPosition = computed(() => {});
 
-  selectTab(tabIndex: number) {
-    this.selectedIndex.set(tabIndex);
+  selectTab(tabIndex: any) {
+    this.selectedIndex.set(tabIndex[0]);
     this.moveInk();
   }
 
-  private moveInk() {}
+  private moveInk() {
+    const selectedIndex = this.selectedIndex();
+    const tabMainHeader = this.tabMainHeader()?.nativeElement;
+    const tabHeader = this.tabHeaders()[selectedIndex].nativeElement;
+
+    const tabInk = this.tabInk()?.nativeElement;
+    if (tabHeader && tabMainHeader && tabInk) {
+      const tabRect = tabHeader.getBoundingClientRect();
+      const tabsRect = tabMainHeader.getBoundingClientRect();
+
+      tabInk.style.width = `${tabRect.width}px`;
+      tabInk.style.left = `${tabRect.left - tabsRect.left}px`;
+    }
+  }
 }
