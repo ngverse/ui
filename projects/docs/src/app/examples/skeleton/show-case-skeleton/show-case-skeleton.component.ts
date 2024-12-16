a0301a347ab705229e3dfdbd167c172b6aa29025
lukonik <81145822+lukonik@users.noreply.github.com>
2024-12-16 11:25:03 +0400

    feat: skeleton implemented (#11)

---
+++ projects/docs/src/app/examples/skeleton/show-case-skeleton/show-case-skeleton.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { SkeletonComponent } from '@ng-verse/skeleton/skeleton.component';
+
+@Component({
+  selector: 'doc-show-case-skeleton',
+  imports: [SkeletonComponent],
+  templateUrl: './show-case-skeleton.component.html',
+  styleUrl: './show-case-skeleton.component.scss',
+})
+export class ShowCaseSkeletonComponent {}
--- projects/docs/src/app/features/skeleton-page/skeleton-page.component.ts
+++ projects/docs/src/app/features/skeleton-page/skeleton-page.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { BlueprintPageComponent } from '../../blueprint/blueprint-page/blueprint-page.component';
 import { ShowCaseComponent } from '../../blueprint/show-case/show-case.component';
 import { SkeletonComponent } from '../../../../../ng-verse/src/lib/skeleton/skeleton.component';
+import { ShowCaseSkeletonComponent } from '../../examples/skeleton/show-case-skeleton/show-case-skeleton.component';
+import { CommandInstallationComponent } from '../../blueprint/command-installation/command-installation.component';
+import { SourceTreeComponent } from '../../blueprint/source-tree/source-tree.component';
+import { ApiInputsComponent } from '../../blueprint/api-info/api-inputs/api-inputs.component';
+import { ApiInfoComponent } from '../../blueprint/api-info/api-info.component';
+import { SourceTreeBuilder } from '../../blueprint/source-tree/source-tree-builder';
+import { ApiDescriptionComponent } from "../../blueprint/api-info/api-description/api-description.component";
 
 @Component({
   selector: 'doc-skeleton-page',
-  imports: [BlueprintPageComponent, ShowCaseComponent, SkeletonComponent],
+  imports: [
+    BlueprintPageComponent,
+    ShowCaseComponent,
+    CommandInstallationComponent,
+    ShowCaseSkeletonComponent,
+    SourceTreeComponent,
+    ApiInputsComponent,
+    ApiInfoComponent,
+    ApiDescriptionComponent
+],
   templateUrl: './skeleton-page.component.html',
   styleUrl: './skeleton-page.component.scss',
 })
-export class SkeletonPageComponent {}
+export class SkeletonPageComponent {
+  sourceTreeBuilder = inject(SourceTreeBuilder);
+
+  sourceTree = this.sourceTreeBuilder.sourceTree('skeleton', (root) => [
+    this.sourceTreeBuilder.folder(
+      root,
+      root,
+      () => this.sourceTreeBuilder.fullComponent('skeleton', root),
+      true
+    ),
+  ]);
+}
