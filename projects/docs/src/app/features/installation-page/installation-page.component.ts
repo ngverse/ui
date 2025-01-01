b39fa5a37a1ea187e23de67cf8aefaf61be07752
luka onik <luka.onikadze@toptal.com>
2025-01-01 15:25:48 +0400

    add introduction and installation

--- projects/docs/src/app/features/installation-page/installation-page.component.ts
+++ projects/docs/src/app/features/installation-page/installation-page.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
-import { BlogH3Component } from "../../blog/blog-h3/blog-h3.component";
-import { BlogH4Component } from "../../blog/blog-h4/blog-h4.component";
-import { BlogPComponent } from "../../blog/blog-p/blog-p.component";
 import { BlogPageComponent } from '../../blog/blog-page/blog-page.component';
-import { SourceCodeComponent } from "../../blueprint/source-code/source-code.component";
-import { ProjectNameComponent } from "../../core/project-name/project-name.component";
+import { SourceCodeComponent } from '../../blueprint/source-code/source-code.component';
 
 @Component({
   selector: 'doc-installation-page',
-  imports: [BlogPageComponent, BlogH3Component, SourceCodeComponent, BlogH4Component, BlogPComponent, ProjectNameComponent],
+  imports: [BlogPageComponent, SourceCodeComponent],
   templateUrl: './installation-page.component.html',
   styleUrl: './installation-page.component.scss',
 })
-export class InstallationPageComponent {}
+export class InstallationPageComponent {
+  animationsCode = `import { provideAnimations } from '@angular/platform-browser/animations';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+       provideAnimations(),
+     ],
+};`;
+}
