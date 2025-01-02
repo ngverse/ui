f75f7cf12cab4b9653bc79c4eae61ccc31d2328d
luka onik <luka.onikadze@toptal.com>
2025-01-02 15:43:30 +0400

    fix bugs

--- projects/ng-verse/schematics/ng-add/index.ts
+++ projects/ng-verse/schematics/ng-add/index.ts
@@ -1,122 +1,31 @@
-import {
-  callRule,
-  chain,
-  noop,
-  Rule,
-  SchematicContext,
-  Tree,
-} from '@angular-devkit/schematics';
-import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
-import {
-  getProjectFromWorkspace,
-  getProjectStyleFile,
-} from '@angular/cdk/schematics';
-import { addRootProvider } from '@schematics/angular/utility';
-import { join } from 'path';
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.dev/license
+ */
 
+import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import {
-  addPackageJsonDependency,
-  NodeDependencyType,
-} from '@schematics/angular/utility/dependencies';
-import { getWorkspace } from '@schematics/angular/utility/workspace';
+  NodePackageInstallTask,
+  RunSchematicTask,
+} from '@angular-devkit/schematics/tasks';
+
+import { addPackageToPackageJson } from './package-config';
 import { Schema } from './schema';
-
-function installDependenceis(): Rule {
-  return (tree: Tree, context: SchematicContext) => {
-    const packageJson = JSON.parse(
-      tree.read('package.json')?.toString() || '{}'
-    );
-    if (!packageJson.dependencies['@angular/cdk']) {
-      addPackageJsonDependency(tree, {
-        type: NodeDependencyType.Default,
-        name: '@angular/cdk',
-        version: '>=19.0.1',
-      });
-      context.addTask(new NodePackageInstallTask());
-    }
-    if (!packageJson.dependencies['ng-verse']) {
-      addPackageJsonDependency(tree, {
-        type: NodeDependencyType.Default,
-        name: 'ng-verse',
-        version: 'latest',
-      });
-      context.addTask(new NodePackageInstallTask());
-    }
-    return tree;
-  };
-}
-
-function addStyles(options: Schema) {
-  return async (host: Tree, context: SchematicContext) => {
-    const workspace = await getWorkspace(host);
-    const project = getProjectFromWorkspace(workspace, options.project);
-    const styleFilePath = getProjectStyleFile(project);
-    const logger = context.logger;
-
-    if (!styleFilePath) {
-      logger.error(`Could not find the default style file for this project.`);
-      logger.info(`Consider manually adding the Roboto font to your CSS.`);
-      logger.info(
-        `More information at https://fonts.google.com/specimen/Roboto`
-      );
-      return;
-    }
-
-    const buffer = host.read(styleFilePath);
-
-    if (!buffer) {
-      logger.error(
-        `Could not read the default style file within the project ` +
-          `(${styleFilePath})`
-      );
-      logger.info(`Please consider manually setting up the Roboto font.`);
-      return;
-    }
-
-    const htmlContent = buffer.toString();
-    const globalStylesPath = join(
-      'node_modules',
-      'ng-verse',
-      'src',
-      'lib',
-      'globals.scss'
-    );
-    const globalStylesContent = host.read(globalStylesPath);
-
-    const insertion = `\n ${globalStylesContent?.toString() || ''}\n`;
-
-    if (htmlContent.includes(insertion)) {
-      return;
-    }
-
-    const recorder = host.beginUpdate(styleFilePath);
-
-    recorder.insertLeft(htmlContent.length, insertion);
-    host.commitUpdate(recorder);
-  };
-}
-
-function addAnimations(options: Schema) {
-  return (host: Tree, context: SchematicContext) => {
-    const animationsRule =
-      options.animations === 'excluded'
-        ? noop()
-        : addRootProvider(options.project, ({ code, external }) => {
-            return code`${external(
-              'provideAnimationsAsync',
-              '@angular/platform-browser/animations/async'
-            )}(${options.animations === 'disabled' ? `'noop'` : ''})`;
-          });
-
-    // The `addRootProvider` rule can throw in some custom scenarios (see #28640).
-    // Add some error handling around it so the setup isn't interrupted.
-    return callRule(animationsRule as Rule, host, context);
-  };
-}
 export function ngAdd(options: Schema): Rule {
-  return chain([
-    installDependenceis(),
-    addAnimations(options),
-    addStyles(options),
-  ]);
+  return (host: Tree, context: SchematicContext) => {
+    // const angularDependencyVersion = ngCoreVersionTag || `0.0.0-NG`;
+
+    addPackageToPackageJson(host, '@angular/cdk', '>=19.0.1');
+
+    // Since the Angular Material schematics depend on the schematic utility functions from the
+    // CDK, we need to install the CDK before loading the schematic files that import from the CDK.
+    const installTaskId = context.addTask(new NodePackageInstallTask());
+
+    context.addTask(new RunSchematicTask('ng-add-setup-project', options), [
+      installTaskId,
+    ]);
+  };
 }
