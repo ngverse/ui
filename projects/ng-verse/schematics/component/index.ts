db61d901bf7c69cf92b9c9145650472de2747b16
luka onik <luka.onikadze@toptal.com>
2025-01-03 11:37:30 +0400

    fix: normalize paths

--- projects/ng-verse/schematics/component/index.ts
+++ projects/ng-verse/schematics/component/index.ts
@@ -1,3 +1,4 @@
+import { normalize } from '@angular-devkit/core';
 import { SchematicsException, Tree } from '@angular-devkit/schematics';
 import { getProjectFromWorkspace } from '@angular/cdk/schematics';
 import { getWorkspace } from '@schematics/angular/utility/workspace';
@@ -7,7 +8,6 @@ import { Schema } from './schema';
 // A function that will generate a Rule to copy the specified component folder
 export function component(options: Schema) {
   return async (host: Tree) => {
-
     const workspace = await getWorkspace(host);
     const project = getProjectFromWorkspace(workspace, options.project);
 
@@ -17,18 +17,17 @@ export function component(options: Schema) {
     const projectType =
       project.extensions['projectType'] === 'application' ? 'app' : 'lib';
 
-    const rootPath = `${project.sourceRoot}/${projectType}/${
-      options.path ?? '/core/components'
-    }`;
+    // Normalize the paths to ensure consistent formatting across OS
+    const rootPath = normalize(
+      `${project.sourceRoot}/${projectType}/${
+        options.path ?? '/core/components'
+      }`
+    );
 
-    const applicationPath = join(rootPath, options.name);
+    const applicationPath = normalize(join(rootPath, options.name));
 
-    const componentsPath = join(
-      'node_modules',
-      'ng-verse',
-      'src',
-      'lib',
-      options.name
+    const componentsPath = normalize(
+      join('node_modules', 'ng-verse', 'src', 'lib', options.name)
     );
 
     // Copy component files from the library to the application
