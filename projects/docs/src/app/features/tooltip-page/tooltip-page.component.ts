351f0c807f4b273c8706c727150952735d60b944
luka onik <luka.onikadze@toptal.com>
2025-01-04 14:26:10 +0400

    container tests

--- projects/docs/src/app/features/tooltip-page/tooltip-page.component.ts
+++ projects/docs/src/app/features/tooltip-page/tooltip-page.component.ts
@@ -1,16 +1,19 @@
 import { Component, inject } from '@angular/core';
-import { ApiInfoComponent } from '../../blueprint/api-info/api-info.component';
 import {
-    ApiEntity,
-    ApiInputsComponent,
-    EMPTY_API_INPUT_DEFAULT_VALUE,
-} from '../../blueprint/api-info/api-inputs/api-inputs.component';
+  ApiInfo,
+  ApiInfoComponent,
+} from '../../blueprint/api-info/api-info.component';
+import { EMPTY_API_INPUT_DEFAULT_VALUE } from '../../blueprint/api-info/api-inputs/api-inputs.component';
 import { BlueprintPageComponent } from '../../blueprint/blueprint-page/blueprint-page.component';
 import { CommandInstallationComponent } from '../../blueprint/command-installation/command-installation.component';
 import { ShowCaseComponent } from '../../blueprint/show-case/show-case.component';
-import { SourceTreeBuilder } from '../../blueprint/source-tree/source-tree-builder';
+import {
+  SourceTreeBuilder,
+  SourceTreeFolder,
+} from '../../blueprint/source-tree/source-tree-builder';
 import { SourceTreeComponent } from '../../blueprint/source-tree/source-tree.component';
 import { ShowCaseTooltipComponent } from '../../examples/tooltip/show-case-tooltip/show-case-tooltip.component';
+const ROOT = 'tooltip';
 
 @Component({
   selector: 'doc-tooltip-page',
@@ -22,63 +25,68 @@ import { ShowCaseTooltipComponent } from '../../examples/tooltip/show-case-toolt
     ShowCaseTooltipComponent,
     SourceTreeComponent,
     ApiInfoComponent,
-    ApiInputsComponent,
   ],
   templateUrl: './tooltip-page.component.html',
   styleUrl: './tooltip-page.component.scss',
 })
 export class TooltipPageComponent {
   sourceTreeBuilder = inject(SourceTreeBuilder);
-  sourceTree = this.sourceTreeBuilder.sourceTree('tooltip', (root) => [
-    this.sourceTreeBuilder.folder(
-      root,
-      root,
-      () => [this.sourceTreeBuilder.directive('tooltip', root)],
-      true
-    ),
-    this.sourceTreeBuilder.folder(
-      'tooltip-container',
-      `${root}/tooltip-container`,
-      (root) => this.sourceTreeBuilder.fullComponent('tooltip-container', root),
-      false
-    ),
-  ]);
-
-  apiInputs: ApiEntity[] = [
+  sourceTree: SourceTreeFolder[] = [
     {
-      name: 'toolip',
-      inputs: [
-        {
-          name: 'message',
-          type: 'string',
-          description: 'text to display on tooltip',
-          default: EMPTY_API_INPUT_DEFAULT_VALUE,
-        },
-        {
-          name: 'tooltipPosition',
-          type: 'top | right | bottom | left',
-          description: 'tooltip position',
-          default: 'top',
-        },
-        {
-          name: 'tooltipEvent',
-          type: 'hover | focus | both',
-          description: 'defines when to display tooltip',
-          default: 'both',
-        },
-        {
-          name: 'tooltipDelay',
-          type: 'number',
-          description: 'A delay (in milliseconds) before the tooltip appears.',
-          default: '0',
-        },
-        {
-          name: 'tooltipContent',
-          type: 'ng-template',
-          description: 'The content template to be displayed inside the tooltip.',
-          default: EMPTY_API_INPUT_DEFAULT_VALUE,
-        },
-      ],
+      name: ROOT,
+      files: [this.sourceTreeBuilder.directive('tooltip', ROOT)],
+      hideName: true,
+    },
+    {
+      name: 'tooltip-container',
+      files: this.sourceTreeBuilder.fullComponent(
+        'tooltip-container',
+        `${ROOT}/tooltip-container`
+      ),
     },
   ];
+
+  apiInfo: ApiInfo = {
+    entities: [
+      {
+        name: 'TooltipComponent',
+        type: 'directive',
+        selector: '[appTooltip]',
+        inputs: [
+          {
+            name: 'message',
+            type: 'string',
+            description: 'text to display on tooltip',
+            default: EMPTY_API_INPUT_DEFAULT_VALUE,
+          },
+          {
+            name: 'tooltipPosition',
+            type: 'top | right | bottom | left',
+            description: 'tooltip position',
+            default: 'top',
+          },
+          {
+            name: 'tooltipEvent',
+            type: 'hover | focus | both',
+            description: 'defines when to display tooltip',
+            default: 'both',
+          },
+          {
+            name: 'tooltipDelay',
+            type: 'number',
+            description:
+              'A delay (in milliseconds) before the tooltip appears.',
+            default: '0',
+          },
+          {
+            name: 'tooltipContent',
+            type: 'ng-template',
+            description:
+              'The content template to be displayed inside the tooltip.',
+            default: EMPTY_API_INPUT_DEFAULT_VALUE,
+          },
+        ],
+      },
+    ],
+  };
 }
--- projects/ng-verse/src/lib/tooltip/tooltip-container/tooltip-container.component.spec.ts
+++ projects/ng-verse/src/lib/tooltip/tooltip-container/tooltip-container.component.spec.ts
@@ -1,5 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import {
+  AfterViewInit,
+  Component,
+  TemplateRef,
+  viewChild,
+} from '@angular/core';
 import { TooltipContainerComponent } from './tooltip-container.component';
 
 describe('TooltipContainerComponent', () => {
@@ -8,9 +14,8 @@ describe('TooltipContainerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TooltipContainerComponent]
-    })
-    .compileComponents();
+      imports: [TooltipContainerComponent, TooltipContainerTestComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TooltipContainerComponent);
     component = fixture.componentInstance;
@@ -20,4 +25,42 @@ describe('TooltipContainerComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should add position class to the tooltip container', () => {
+    component.position.set('bottom');
+    fixture.detectChanges();
+    const tooltipContainer = fixture.nativeElement.querySelector('.tooltip');
+    expect(tooltipContainer.classList).toContain('bottom');
+  });
+  it('should display message in the tooltip container', () => {
+    component.message.set('Hello World');
+    fixture.detectChanges();
+    const tooltipContainer = fixture.nativeElement.querySelector('.tooltip');
+    expect(tooltipContainer.textContent).toContain('Hello World');
+  });
+  it('should display content in the tooltip container', () => {
+    const fixture = TestBed.createComponent(TooltipContainerTestComponent);
+    fixture.detectChanges();
+    const tooltipContainer = fixture.nativeElement.querySelector('.tooltip');
+    expect(tooltipContainer.textContent).toBe('I am tooltip');
+  });
 });
+
+@Component({
+  imports: [TooltipContainerComponent],
+  template: `
+    <ng-template #temp>I am tooltip</ng-template>
+    <app-tooltip-container></app-tooltip-container>
+  `,
+})
+class TooltipContainerTestComponent implements AfterViewInit {
+  temp = viewChild<TemplateRef<unknown>>('temp');
+  tooltipContainer = viewChild(TooltipContainerComponent);
+
+  ngAfterViewInit(): void {
+    const temp = this.temp();
+    if (temp) {
+      this.tooltipContainer()?.content.set(temp);
+    }
+  }
+}
