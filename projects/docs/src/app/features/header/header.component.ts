b8486135e98a828962cb4deb5f9c6282b3dc1658
luka onik <luka.onikadze@toptal.com>
2025-01-05 20:05:23 +0400

    mobile update

--- projects/docs/src/app/features/header/header.component.ts
+++ projects/docs/src/app/features/header/header.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { ProjectNameComponent } from "../../core/project-name/project-name.component";
+import { Overlay, OverlayRef } from '@angular/cdk/overlay';
+import { ComponentPortal } from '@angular/cdk/portal';
+import { Component, inject } from '@angular/core';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { LucideAngularModule, Menu } from 'lucide-angular';
+import { filter, take, takeUntil } from 'rxjs';
+import { ProjectNameComponent } from '../../core/project-name/project-name.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'doc-header',
-  imports: [RouterLink, ProjectNameComponent],
+  imports: [RouterLink, ProjectNameComponent, LucideAngularModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  Menu = Menu;
+  overlay = inject(Overlay);
+  overlayRef: OverlayRef | undefined;
+  router = inject(Router);
+
+  showSidebar() {
+    this.overlayRef = this.overlay.create({
+      positionStrategy: this.overlay.position().global().left(),
+      // height: '100%',
+      // width: '90vw',
+      hasBackdrop: true,
+      disposeOnNavigation: true,
+    });
+    this.overlayRef.backdropClick().subscribe(() => {
+      this.overlayRef?.dispose();
+    });
+    this.router.events
+      .pipe(filter((e) => e instanceof NavigationEnd))
+      .pipe(take(1))
+      .pipe(takeUntil(this.overlayRef.backdropClick().pipe(take(1))))
+      .subscribe(() => {
+        this.overlayRef?.dispose();
+      });
+    this.overlayRef.attach(new ComponentPortal(SidebarComponent));
+  }
+}
