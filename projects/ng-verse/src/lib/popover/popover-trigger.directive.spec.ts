f524e541b2559efde401dd09507b3cbceff33295
luka onik <luka.onikadze@toptal.com>
2025-01-07 14:09:34 +0400

    silent tests

--- projects/ng-verse/src/lib/popover/popover-trigger.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PopoverTriggerDirective } from './popover-trigger.directive';
-
-describe('PopoverTriggerDirective', () => {
-  it('should create an instance', () => {
-    const directive = new PopoverTriggerDirective();
-    expect(directive).toBeTruthy();
-  });
-});
