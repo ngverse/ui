db8b8dd1bc1085cfee084e92e53916e1ab90f63e
luka onik <luka.onikadze@toptal.com>
2025-01-07 14:50:37 +0400

    add resize input

--- projects/ng-verse/src/lib/textarea/textarea.component.ts
+++ projects/ng-verse/src/lib/textarea/textarea.component.ts
@@ -1,13 +1,24 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+
+type RESIZE_TYPES =
+  | 'none'
+  | 'both'
+  | 'horizontal'
+  | 'vertical'
+  | 'block'
+  | 'inline';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: '[appTextarea]',
   imports: [],
   template: ``,
+  host: {
+    '[style.resize]': 'resize()',
+  },
   styleUrl: './textarea.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TextareaComponent {
-
+  resize = input<RESIZE_TYPES>('none');
 }
