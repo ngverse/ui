099241a47820b463e66d6aa0fe3f2915ab83374b
luka onik <luka.onikadze@toptal.com>
2025-01-07 22:19:02 +0400

    context menu

--- projects/playground/src/app/app.component.ts
+++ projects/playground/src/app/app.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
-import { CheckboxComponent } from '../../../ng-verse/src/lib/checkbox/checkbox.component';
+import {
+  Overlay,
+  STANDARD_DROPDOWN_BELOW_POSITIONS,
+} from '@angular/cdk/overlay';
+import { TemplatePortal } from '@angular/cdk/portal';
+import {
+  Component,
+  inject,
+  TemplateRef,
+  viewChild,
+  ViewContainerRef,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [CheckboxComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'playground';
+  overlay = inject(Overlay);
+  el = viewChild.required('el');
+  temp = viewChild.required(TemplateRef);
+  vf = inject(ViewContainerRef);
+  clicked(event: MouseEvent) {
+    event.preventDefault();
+    const x = event.clientX;
+    const y = event.clientY;
+
+    const overlayRef = this.overlay.create({
+      hasBackdrop: true,
+      backdropClass: 'cdk-backdrop-transparent',
+      positionStrategy: this.overlay
+        .position()
+        .flexibleConnectedTo({ x, y })
+        .withPositions(STANDARD_DROPDOWN_BELOW_POSITIONS),
+    });
+
+    overlayRef.backdropClick().subscribe(() => {
+      overlayRef.dispose();
+    });
+    overlayRef.keydownEvents().subscribe((e) => {
+      if (e.key === 'Escape') {
+        overlayRef.dispose();
+      }
+    });
+
+    const templatePortal = new TemplatePortal(this.temp(), this.vf);
+
+    overlayRef.attach(templatePortal);
+    overlayRef.updatePosition();
+  }
 }
