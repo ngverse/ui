20e71d2ef3654dbccbf32e2c1d41859c4d21b50f
luka onik <luka.onikadze@toptal.com>
2025-01-09 17:10:26 +0400

    remove tests

--- projects/ng-verse/src/lib/listbox/listbox-item.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ListboxItemDirective } from './listbox-item.directive';
-
-describe('ListboxItemDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ListboxItemDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- projects/ng-verse/src/lib/listbox/listbox.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ListboxDirective } from './listbox.directive';
-
-describe('ListboxDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ListboxDirective();
-    expect(directive).toBeTruthy();
-  });
-});
