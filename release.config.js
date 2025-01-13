fa668eba5b783e21bfe7107d2a05820e405ebcca
luka onik <luka.onikadze@toptal.com>
2025-01-13 15:23:58 +0400

    build: add changelog file

--- release.config.js
+++ release.config.js
@@ -4,7 +4,12 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
     [
       '@semantic-release/npm',
       {
