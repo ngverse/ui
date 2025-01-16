2fa10f191b0e2c9f1347608774bc5d37a649ae81
lukonik <81145822+lukonik@users.noreply.github.com>
2025-01-16 19:16:22 +0400

    refactor(card): only keep main card component (#101)

--- projects/docs/src/app/examples/card/show-case-card/show-case-card.component.ts
+++ projects/docs/src/app/examples/card/show-case-card/show-case-card.component.ts
@@ -1,23 +1,12 @@
 import { Component, model } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from '@ng-verse/button/button.component';
-import { CardContentComponent } from '@ng-verse/card/card-content.component';
-import { CardFooterComponent } from '@ng-verse/card/card-footer.component';
-import { CardTitleComponent } from '@ng-verse/card/card-title.component';
 import { CardComponent } from '@ng-verse/card/card.component';
 import { InputComponent } from '@ng-verse/input/input.component';
 
 @Component({
   selector: 'doc-show-case-card',
-  imports: [
-    CardComponent,
-    CardTitleComponent,
-    CardContentComponent,
-    CardFooterComponent,
-    ButtonComponent,
-    FormsModule,
-    InputComponent,
-  ],
+  imports: [CardComponent, ButtonComponent, FormsModule, InputComponent],
   templateUrl: './show-case-card.component.html',
   styleUrl: './show-case-card.component.scss',
 })
--- projects/ng-verse/src/lib/card/card-content.component.ts
+++
@@ -1,8 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-card-content',
-  template: ` <ng-content></ng-content> `,
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class CardContentComponent {}
