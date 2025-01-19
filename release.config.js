878b7a604a0b686e4bce5674c6f2b0e66a6d090b
luka onik <luka.onikadze@toptal.com>
2025-01-19 21:52:17 +0400

    fix: add correct config to release

--- release.config.js
+++ release.config.js
@@ -1,30 +1,16 @@
 module.exports = {
-  branches: ['main'], // Specify the branch to release from
-  repositoryUrl: 'https://github.com/lukonik/ng-verse', // Replace with your repo URL
+  branches: ['main'],
+  repositoryUrl: 'https://github.com/lukonik/ng-verse',
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     [
-      '@semantic-release/exec',
-      {
-        prepareCmd:
-          'ng build --project=ng-verse && cd projects/ng-verse && npm run build && npm pack',
-      },
-    ],
-    [
       '@semantic-release/npm',
       {
-        pkgRoot: 'dist/ng-verse',
+        pkgRoot: 'projects/ng-verse',
       },
     ],
     '@semantic-release/git',
-    [
-      '@semantic-release/github',
-      {
-        assets: [
-          { path: 'projects/ng-verse/dist/*.tgz', label: 'NPM Package' },
-        ],
-      },
-    ],
+    ['@semantic-release/github'],
   ],
 };
