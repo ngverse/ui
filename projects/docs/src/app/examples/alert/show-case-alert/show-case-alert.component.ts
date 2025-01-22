503b9fc2d05ff6f72c66ee73b49f6f6ad040a42f
lukonik <81145822+lukonik@users.noreply.github.com>
2025-01-22 22:40:48 +0400

    feat(alert): alert stable (#142)

--- projects/docs/src/app/examples/alert/show-case-alert/show-case-alert.component.ts
+++ projects/docs/src/app/examples/alert/show-case-alert/show-case-alert.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { AlertBodyComponent } from '@ng-verse/alert/alert-body/alert-body.component';
-import { AlertHeaderComponent } from '@ng-verse/alert/alert-header/alert-header.component';
+import { AlertBodyComponent } from '@ng-verse/alert/alert-body.component';
+import { AlertHeaderComponent } from '@ng-verse/alert/alert-header.component';
 import { AlertComponent } from '@ng-verse/alert/alert.component';
 
 @Component({
--- projects/ng-verse/src/lib/alert/alert-header.component.ts
+++ projects/ng-verse/src/lib/alert/alert-header.component.ts
@@ -3,8 +3,13 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'app-alert-header',
   imports: [],
-  templateUrl: './alert-header.component.html',
-  styleUrl: './alert-header.component.scss',
+  template: `<ng-content></ng-content> `,
+  styles: `
+    :host {
+      font-weight: 500;
+      display: block;
+    }
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AlertHeaderComponent {}
