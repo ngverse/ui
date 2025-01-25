5552f2850533273533ba897a828fbc552bd59df3
Nikolay <nikolay.laz@gmail.com>
2025-01-25 17:00:30 +0200

    docs(dialog): updated docs and added tests for dialog (#154)

--- projects/docs/src/app/features/dialog-page/dialog-page.component.ts
+++ projects/docs/src/app/features/dialog-page/dialog-page.component.ts
@@ -12,6 +12,7 @@ import {
 } from '../../blueprint/source-tree/source-tree-builder';
 import { SourceTreeComponent } from '../../blueprint/source-tree/source-tree.component';
 import { ShowCaseDialogComponent } from '../../examples/dialog/show-case-dialog/show-case-dialog.component';
+
 const ROOT = 'dialog';
 @Component({
   selector: 'doc-dialog-page',
@@ -35,16 +36,181 @@ export class DialogPageComponent {
       files: [
         ...this.sourceTreeBuilder.service('dialog', ROOT),
         ...this.sourceTreeBuilder.fullComponent('dialog', `${ROOT}/dialog`),
+        ...this.sourceTreeBuilder.fullComponent(
+          'alert-dialog',
+          `${ROOT}/alert-dialog`
+        ),
+        ...this.sourceTreeBuilder.fullComponent(
+          'confirm-dialog',
+          `${ROOT}/confirm-dialog`
+        ),
+        ...this.sourceTreeBuilder.directive('dialog-close', `${ROOT}`, true),
+        this.sourceTreeBuilder.component('dialog-close-icon', `${ROOT}`),
       ],
       hideName: true,
     },
   ];
 
   apiInfo: ApiInfo = {
+    ariaLink: 'https://material.angular.io/cdk/dialog/overview#accessibility',
     entities: [
       {
         name: 'DialogService',
         type: 'service',
+        description: 'service that opens a dialog',
+        methods: [
+          {
+            name: 'dialog',
+            description: 'opens the dialog',
+            returnType: 'DialogReturn<T>',
+            params: [
+              {
+                name: 'component',
+                type: 'ComponentType<unknown>',
+                description: 'component to render inside the dialog.',
+              },
+              {
+                name: 'options',
+                type: 'object',
+                fields: [
+                  {
+                    name: 'data',
+                    type: 'unknown',
+                    description: 'data to pass to the component.',
+                  },
+                  {
+                    name: 'title',
+                    type: 'string',
+                    description: 'title of the drawer.',
+                  },
+                  {
+                    name: 'hasBackdrop',
+                    type: 'boolean',
+                    description: 'whether the dialog has a backdrop.',
+                    default: 'true',
+                  },
+                  {
+                    name: 'disableClose',
+                    type: 'boolean',
+                    description:
+                      'whether the dialog closes with the escape key or pointer events outside the panel element.',
+                    default: 'false',
+                  },
+                  {
+                    name: 'showClose',
+                    type: 'boolean',
+                    description: 'whether to show close icon',
+                    default: 'true',
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            name: 'confirm',
+            description: 'opens a confirm dialog',
+            returnType: 'DialogReturn<T>',
+            params: [
+              {
+                name: 'options',
+                type: 'object',
+                fields: [
+                  {
+                    name: 'title',
+                    type: 'string',
+                    description: 'title of the dialog.',
+                  },
+                  {
+                    name: 'description',
+                    type: 'string',
+                    description: 'description of the dialog.',
+                  },
+                  {
+                    name: 'yesLabel',
+                    type: 'string',
+                    description: 'confirm button label',
+                    default: 'Yes',
+                  },
+                  {
+                    name: 'noLabel',
+                    type: 'string',
+                    description: 'cancel/reject button label',
+                    default: 'No',
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            name: 'alert',
+            description: 'opens a alert dialog',
+            returnType: 'DialogReturn<T>',
+            params: [
+              {
+                name: 'options',
+                type: 'object',
+                fields: [
+                  {
+                    name: 'title',
+                    type: 'string',
+                    description: 'title of the alert dialog.',
+                  },
+                  {
+                    name: 'description',
+                    type: 'string',
+                    description: 'description of the alert dialog.',
+                  },
+                  {
+                    name: 'buttonLabel',
+                    type: 'string',
+                    description: 'alert button label',
+                    default: 'OK',
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: 'DialogCloseDirective',
+        selector: '[appDialogClose]',
+        type: 'directive',
+        description:
+          'A directive used to close the dialog. It can be placed on any element inside the dialog.',
+        inputs: [
+          {
+            name: 'appDialogClose',
+            type: 'unknown',
+            description: 'value to pass to the close method',
+          },
+        ],
+      },
+      {
+        name: 'ConfirmDirective',
+        selector: '[appConfirm]',
+        type: 'directive',
+        description:
+          'A directive used to open a confirm dialog. It can be placed on any element.',
+        inputs: [
+          {
+            name: 'confirmOptions',
+            type: 'object',
+            description: 'object with options for the dialog',
+          },
+        ],
+        outputs: [
+          {
+            name: 'approved',
+            description: 'emits when the dialog is approved',
+            value: 'void',
+          },
+          {
+            name: 'rejected',
+            description: 'emits when the dialog is rejected',
+            value: 'void',
+          },
+        ],
       },
     ],
   };
