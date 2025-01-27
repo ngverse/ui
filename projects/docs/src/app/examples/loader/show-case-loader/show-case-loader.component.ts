7ac0710351baf7b2509ffd168f47af0795eb3b41
luka onik <luka.onikadze@toptal.com>
2025-01-28 00:24:41 +0400

    feat(loader): loader stable

--- projects/docs/src/app/examples/loader/show-case-loader/show-case-loader.component.ts
+++ projects/docs/src/app/examples/loader/show-case-loader/show-case-loader.component.ts
@@ -1,10 +1,20 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { ButtonComponent } from '@ng-verse/button/button.component';
+import { CardComponent } from '@ng-verse/card/card.component';
+import { FormFieldComponent } from '@ng-verse/form-field/form-field.component';
+import { InputComponent } from '@ng-verse/input/input.component';
 import { LoaderComponent } from '@ng-verse/loader/loader.component';
 
 @Component({
   selector: 'doc-show-case-loader',
-  imports: [LoaderComponent, ButtonComponent],
+  imports: [
+    LoaderComponent,
+    ButtonComponent,
+    FormFieldComponent,
+    InputComponent,
+    ButtonComponent,
+    CardComponent,
+  ],
   templateUrl: './show-case-loader.component.html',
   styleUrl: './show-case-loader.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
