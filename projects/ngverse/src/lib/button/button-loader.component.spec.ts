14c3ad648d07765c83170969d9d3fcc94fc299a7
luka onik <luka.onikadze@toptal.com>
2025-01-29 21:06:35 +0400

    test(button): to zoneless

--- projects/ngverse/src/lib/button/button-loader.component.spec.ts
+++ projects/ngverse/src/lib/button/button-loader.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { ButtonLoaderComponent } from './button-loader.component';
 
 describe('ButtonComponent', () => {
@@ -8,6 +9,7 @@ describe('ButtonComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [ButtonLoaderComponent],
+      providers: [provideExperimentalZonelessChangeDetection()],
     });
     fixture = TestBed.createComponent(ButtonLoaderComponent);
   });
