7f90ff88908e9b260d511accd58d219f0d9b4286
luka onik <luka.onikadze@toptal.com>
2025-01-29 22:14:36 +0400

    test(textarea): to zoneless

--- projects/ngverse/src/lib/textarea/textarea.component.spec.ts
+++ projects/ngverse/src/lib/textarea/textarea.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { TextareaComponent } from './textarea.component';
 
 describe('TextareaComponent', () => {
@@ -9,11 +10,11 @@ describe('TextareaComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [TextareaComponent],
+      providers: [provideExperimentalZonelessChangeDetection()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TextareaComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
