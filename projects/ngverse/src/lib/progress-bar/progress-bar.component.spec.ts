a961fb920eed220f5c4233766e7fe4610c6b450a
luka onik <luka.onikadze@toptal.com>
2025-01-29 22:06:12 +0400

    test(progress-bar): to zoneless

--- projects/ngverse/src/lib/progress-bar/progress-bar.component.spec.ts
+++ projects/ngverse/src/lib/progress-bar/progress-bar.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ComponentRef } from '@angular/core';
+import {
+  ComponentRef,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
 import { ProgressBarComponent } from './progress-bar.component';
 
 describe('ProgressBarComponent', () => {
@@ -11,21 +14,21 @@ describe('ProgressBarComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ProgressBarComponent],
+      providers: [provideExperimentalZonelessChangeDetection()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProgressBarComponent);
     component = fixture.componentInstance;
     componentRef = fixture.componentRef;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it("should display indeterminate progress bar if 'indeterminate' is true", () => {
+  it("should display indeterminate progress bar if 'indeterminate' is true", async () => {
     componentRef.setInput('indeterminate', true);
-    fixture.detectChanges();
+    await fixture.whenStable();
     const element = fixture.nativeElement.querySelector('.indeterminate-line');
     expect(element).toBeTruthy();
   });
