de2ff2eb512b80769d2b31c4ca1fea90a49d4c0b
lukonik <81145822+lukonik@users.noreply.github.com>
2025-01-31 16:47:59 +0400

    docs: add key features (#188)

--- projects/docs/src/app/features/doc-container-page/doc-container-page.component.ts
+++ projects/docs/src/app/features/doc-container-page/doc-container-page.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToastService } from '@ngverse/toast/toast.service';
 import { DocSiblingNavigationsComponent } from '../doc-sibling-navigations/doc-sibling-navigations.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
+const EARLY_RELEASE_KEY = 'early-release';
 
 @Component({
   selector: 'doc-doc-container-page',
@@ -9,4 +11,20 @@ import { SidebarComponent } from '../sidebar/sidebar.component';
   templateUrl: './doc-container-page.component.html',
   styleUrl: './doc-container-page.component.scss',
 })
-export class DocContainerPageComponent {}
+export class DocContainerPageComponent {
+  toast = inject(ToastService);
+  constructor() {
+    afterNextRender(() => {
+      const hasValue = localStorage.getItem(EARLY_RELEASE_KEY);
+      if (!hasValue) {
+        this.toast.open({
+          position: 'top_center',
+          message: 'We are in early release. expect updates !',
+          closeDelay: 3000,
+          showCloseIcon: false,
+        });
+      }
+      localStorage.setItem(EARLY_RELEASE_KEY, 'true');
+    });
+  }
+}
