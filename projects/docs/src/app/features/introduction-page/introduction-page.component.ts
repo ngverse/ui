6cc66381a05484cbc78cb3e98636b60d8ccaeaa0
luka onik <luka.onikadze@toptal.com>
2025-01-31 12:30:35 +0400

    docs: introduction page finished

--- projects/docs/src/app/features/introduction-page/introduction-page.component.ts
+++ projects/docs/src/app/features/introduction-page/introduction-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { BlogPageComponent } from '../../blog/blog-page/blog-page.component';
+import { SourceCodeComponent } from '../../blueprint/source-code/source-code.component';
 
 @Component({
   selector: 'doc-introduction-page',
-  imports: [BlogPageComponent],
+  imports: [BlogPageComponent, SourceCodeComponent],
   templateUrl: './introduction-page.component.html',
   styleUrl: './introduction-page.component.scss',
 })
