42b5ee7f655e04ffcf3fca7f77136d20170bef21
lukonik <81145822+lukonik@users.noreply.github.com>
2025-02-03 17:02:53 +0400

    Button icon (#201)
    * feat(button): add icon-button
    
    * feat(button): add icon button

--- projects/docs/src/app/examples/button/show-case-button/show-case-button.component.ts
+++ projects/docs/src/app/examples/button/show-case-button/show-case-button.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '@ngverse/button/button.component';
+import { IconButtonComponent } from '@ngverse/button/icon-button.component';
+import { IconComponent } from '@ngverse/icon/icon.component';
 
 @Component({
   selector: 'doc-show-case-button',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, IconButtonComponent, IconComponent],
   templateUrl: './show-case-button.component.html',
   styleUrl: './show-case-button.component.scss',
 })
