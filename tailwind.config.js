639e75ec57e2d643062d7009ce4190e303e95ee9
lukonik <81145822+lukonik@users.noreply.github.com>
2025-02-03 12:01:22 +0400

    docs: enable preflight (#200)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./projects/docs/src/**/*.{html,ts}'],
-  corePlugins: {
-    preflight: false,
-  },
   theme: {
     extend: {
       colors: {
