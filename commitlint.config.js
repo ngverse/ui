3b4886a7f0104c74c03a65395ba31a7d88d3e1f9
lukonik <81145822+lukonik@users.noreply.github.com>
2025-02-05 18:28:26 +0400

    feat(local-storage): local-storage implemented (#214)

--- commitlint.config.js
+++ commitlint.config.js
@@ -38,6 +38,7 @@ module.exports = {
         'drawer',
         'roving-listbox',
         'desc-listbox',
+        'local-storage',
       ],
     ],
   },
--- projects/docs/src/app/features/sidebar/sidebar.component.ts
+++ projects/docs/src/app/features/sidebar/sidebar.component.ts
@@ -19,164 +19,142 @@ export const SIDEBAR_ROUTES = [
       {
         name: 'Introduction',
         url: '/doc/introduction',
-        done: true,
       },
       {
         name: 'Installation',
         url: '/doc/installation',
-        done: true,
       },
       {
         name: 'Usage',
         url: '/doc/usage',
-        done: true,
       },
       {
         name: 'Schematics Usage',
         url: '/doc/schematics-usage',
-        done: true,
       },
       {
         name: 'Theming',
         url: '/doc/theming',
-        done: true,
       },
     ],
   },
   {
-    name: 'Elements',
+    name: 'UI',
     children: [
       {
         name: 'Accordion',
         url: '/doc/accordion',
-        done: true,
       },
       {
         name: 'Alert',
         url: '/doc/alert',
-        done: true,
       },
       {
         name: 'Button',
         url: '/doc/button',
-        done: true,
       },
       {
         name: 'Badge',
         url: '/doc/badge',
-        done: true,
       },
       {
         name: 'Drawer',
         url: '/doc/drawer',
-        done: true,
       },
       {
         name: 'Checkbox',
         url: '/doc/checkbox',
-        done: true,
       },
       {
         name: 'Card',
         url: '/doc/card',
-        done: true,
       },
       {
         name: 'Divider',
         url: '/doc/divider',
-        done: true,
       },
       {
         name: 'Skeleton',
         url: '/doc/skeleton',
-        done: true,
       },
       {
         name: 'Loader',
         url: '/doc/loader',
-        done: true,
       },
       {
         name: 'Radio Button',
         url: '/doc/radio-button',
-        done: true,
       },
       {
         name: 'Switch',
         url: '/doc/switch',
-        done: true,
       },
       {
         name: 'Toast',
         url: '/doc/toast',
-        done: true,
       },
       {
         name: 'Tooltip',
         url: '/doc/tooltip',
-        done: true,
       },
       {
         name: 'Textarea',
         url: '/doc/textarea',
-        done: true,
       },
       {
         name: 'Select',
         url: '/doc/select',
-        done: true,
       },
       {
         name: 'Progress Bar',
         url: '/doc/progress-bar',
-        done: true,
       },
       {
         name: 'Dialog',
         url: '/doc/dialog',
-        done: true,
       },
       {
         name: 'Tab',
         url: '/doc/tab',
-        done: true,
       },
       {
         name: 'Input',
         url: '/doc/input',
-        done: true,
       },
       {
         name: 'Form Field',
         url: '/doc/form-field',
-        done: true,
       },
       {
         name: 'OTP Input',
         url: '/doc/otp-input',
-        done: true,
       },
       {
         name: 'Icon',
         url: '/doc/icon',
-        done: true,
       },
       {
         name: 'Popover',
         url: '/doc/popover',
-        done: true,
       },
       {
         name: 'Listbox',
         url: '/doc/listbox',
-        done: true,
       },
       {
         name: 'Context Menu',
         url: '/doc/context-menu',
-        done: true,
       },
     ].sort((a, b) => a.name.localeCompare(b.name)),
+  },
+  {
+    name: 'Utils',
+    children: [
+      {
+        name: 'LocalStorage',
+        url: '/doc/local-storage',
+      },
+    ],
   },
 ] as Group[];
 
