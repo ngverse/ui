5297ae9e18c334d3aa4c07b94baf137dd1d7fd94
luka onik <luka.onikadze@toptal.com>
2025-02-25 00:00:07 +0400

    docs: remove early release toast

--- projects/docs/src/app/features/doc-container-page/doc-container-page.component.ts
+++ projects/docs/src/app/features/doc-container-page/doc-container-page.component.ts
@@ -1,9 +1,7 @@
-import { afterNextRender, Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ToastService } from '@ng-verse/toast/toast.service';
 import { DocSiblingNavigationsComponent } from '../doc-sibling-navigations/doc-sibling-navigations.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
-const EARLY_RELEASE_KEY = 'early-release';
 
 @Component({
   selector: 'doc-doc-container-page',
@@ -11,22 +9,4 @@ const EARLY_RELEASE_KEY = 'early-release';
   templateUrl: './doc-container-page.component.html',
   styleUrl: './doc-container-page.component.scss',
 })
-export class DocContainerPageComponent {
-  toast = inject(ToastService);
-  constructor() {
-    afterNextRender(() => {
-      setTimeout(() => {
-        const hasValue = localStorage.getItem(EARLY_RELEASE_KEY);
-        if (!hasValue) {
-          this.toast.open({
-            position: 'top_center',
-            message: 'We are in early release. expect updates !',
-            closeDelay: 3000,
-            showCloseIcon: false,
-          });
-        }
-        localStorage.setItem(EARLY_RELEASE_KEY, 'true');
-      }, 1000);
-    });
-  }
-}
+export class DocContainerPageComponent {}
